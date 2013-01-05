e66dd87d0016e9367d921e8f6fa03ec1eb2d7a95
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-01-05 11:35:24 +0100

    Easyer to read the code :)

--- application/routes.php
+++ application/routes.php
@@ -40,58 +40,67 @@ Route::get('/', function()
 Route::get('v0/forum', function()
 {
     return Response::eloquent(
-        Forum::query()->order_by('left_id')->get(
-            array(
-                'forum_id as id',
-                'left_id as order',
-                'forum_name as name',
-                'forum_desc as desc',
-                'forum_posts as posts',
-                'forum_topics as topics',
-                'forum_last_post_id as last_post_id',
-                'forum_last_post_time as last_post_time',
-                'forum_last_post_subject as last_post_topic',
-                'forum_last_poster_name as last_poster',
-                'forum_last_poster_id as last_poster_id'
+        Forum::query()
+            ->order_by('left_id')
+            ->get(
+                array(
+                    'forum_id as id',
+                    'left_id as order',
+                    'forum_name as name',
+                    'forum_desc as desc',
+                    'forum_posts as posts',
+                    'forum_topics as topics',
+                    'forum_last_post_id as last_post_id',
+                    'forum_last_post_time as last_post_time',
+                    'forum_last_post_subject as last_post_topic',
+                    'forum_last_poster_name as last_poster',
+                    'forum_last_poster_id as last_poster_id'
+                )
             )
-        )
     );
 });
 
 Route::get('v0/forum/(:num)', function($id)
 {
     return Response::eloquent(
-        Topic::query()->where('forum_id','=', $id)->where('topic_approved','=','1')->order_by('topic_last_post_time', 'desc')->get(
-            array(
-                'topic_id as id',
-                'topic_attachment as attachment',
-                'topic_title as title',
-                'topic_views as views',
-                'topic_replies as topic_replies',
-                'topic_first_poster_name as first_poster',
-                'topic_poster as first_poster_id',
-                'topic_time as first_post_time',
-                'topic_last_poster_name as last_poster',
-                'topic_last_poster_id as last_poster_id',
-                'topic_last_post_time as last_post_time'
+        Topic::query()
+            ->where('forum_id','=', $id)
+            ->where('topic_approved','=','1')
+            ->order_by('topic_last_post_time', 'desc')
+            ->get(
+                array(
+                    'topic_id as id',
+                    'topic_attachment as attachment',
+                    'topic_title as title',
+                    'topic_views as views',
+                    'topic_replies as topic_replies',
+                    'topic_first_poster_name as first_poster',
+                    'topic_poster as first_poster_id',
+                    'topic_time as first_post_time',
+                    'topic_last_poster_name as last_poster',
+                    'topic_last_poster_id as last_poster_id',
+                    'topic_last_post_time as last_post_time'
+                )
             )
-        )
     );
 });
 
 Route::get('v0/user', function()
 {
     return Response::eloquent(
-        User::query()->where('user_password','!=','')->order_by('user_id')->get(
-            array(
-                'user_id as id',
-                'user_regdate as regdate',
-                'username as name',
-                'user_posts as posts',
-                'user_lastvisit as last_visit',
-                'user_lastpost_time as last_post'
+        User::query()
+            ->where('user_password','!=','')
+            ->order_by('user_id')
+            ->get(
+                array(
+                    'user_id as id',
+                    'user_regdate as regdate',
+                    'username as name',
+                    'user_posts as posts',
+                    'user_lastvisit as last_visit',
+                    'user_lastpost_time as last_post'
+                )
             )
-        )
     );
 });
 
