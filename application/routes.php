7d4b2932383b1c2747c35cc401066065987afd02
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-01-12 00:29:59 +0100

    Remove double commented code

--- application/routes.php
+++ application/routes.php
@@ -51,32 +51,7 @@ Route::get('v0', function()
 });
 
 Route::any('v0/forum/(:num?)', array('uses' => 'v0_forum@index'));
-/*
-Route::get('v0/forum/(:num)', function($id)
-{
-    return Response::eloquent(
-        Topic::query()
-            ->where('forum_id','=', $id)
-            ->where('topic_approved','=','1')
-            ->order_by('topic_last_post_time', 'desc')
-            ->get(
-                array(
-                    'topic_id as id',
-                    'topic_attachment as attachment',
-                    'topic_title as title',
-                    'topic_views as views',
-                    'topic_replies as topic_replies',
-                    'topic_first_poster_name as first_poster',
-                    'topic_poster as first_poster_id',
-                    'topic_time as first_post_time',
-                    'topic_last_poster_name as last_poster',
-                    'topic_last_poster_id as last_poster_id',
-                    'topic_last_post_time as last_post_time'
-                )
-            )
-    );
-});
-*/
+
 Route::get('v0/user', function()
 {
     return Response::eloquent(
