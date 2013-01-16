f5261489ec9d81c18a8d87a87b940081fa638e56
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-01-16 10:03:17 +0100

    Add Topics to stats

--- application/libraries/stats.php
+++ application/libraries/stats.php
@@ -7,6 +7,8 @@ class Stats {
             $output = array_merge( $output, Array("posts" => Post::query()->count()));
         if( !$object || $object == "users")
             $output = array_merge( $output, Array("users" => User::query()->count()));
+        if( !$object || $object == "topics")
+            $output = array_merge( $output, Array("topics" => Topic::query()->count()));
         return $output;
     }
 }
