29ac3350f1a61ab5dc01c111b3a9e450f5164a7d
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-01-17 23:50:34 +0100

    Improve getPosts function

--- application/libraries/users.php
+++ application/libraries/users.php
@@ -24,20 +24,22 @@ class Users {
         }
     }
     
-    public static function getPosts($userId)
+    public static function getPosts($userId, $limit = 10)
     {
         return Post::query()
             ->where('poster_id','=', $userId)
             ->where('post_approved','=','1')
             ->order_by('post_time')
+            ->take($limit)
             ->get(
                 array(
                     'post_id as id',
                     'post_attachment as attachment',
                     'post_text as text',
+                    'post_subject as subject',
                     'post_checksum as checksum',
                     'poster_id as poster_id',
-                    'post_time as post_time'
+                    'post_time as time'
                 )
             );
     }
