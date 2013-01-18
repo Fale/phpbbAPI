42ddcb71d5338533cb178a6f0f08c6340dc60c4d
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-01-18 02:26:33 +0100

    Add RSS Topic

--- application/routes/rss.php
+++ application/routes/rss.php
@@ -1,4 +1,5 @@
 <?php
 Route::any('/user/(:num)/(:num?)', array('uses' => 'rss.user@index'));
 Route::any('/forum/(:num)/(:num?)', array('uses' => 'rss.forum@index'));
+Route::any('/topic/(:num)/(:num?)', array('uses' => 'rss.topic@index'));
 ?>
