fcbcb8ad420492579345dc77b768a1b4093c514b
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-01-18 02:32:33 +0100

    Enchant the forum library possibilities

--- application/libraries/forums.php
+++ application/libraries/forums.php
@@ -46,24 +46,45 @@ class Forums {
         }
     }
 
-    public static function getPosts($forumId, $limit = 10)
+    public static function getPosts($forumId = NULL, $limit = 10)
     {
-        return Post::query()
-            ->where('forum_id','=', $forumId)
-            ->where('post_approved','=','1')
-            ->order_by('post_time', 'desc')
-            ->take($limit)
-            ->get(
-                array(
-                    'post_id as id',
-                    'post_attachment as attachment',
-                    'post_text as text',
-                    'post_subject as subject',
-                    'post_checksum as checksum',
-                    'topic_id as topic_id',
-                    'post_time as time'
-                )
-            );
+        if( $forumId )
+        {
+            return Post::query()
+                ->where('forum_id','=', $forumId)
+                ->where('post_approved','=','1')
+                ->order_by('post_time', 'desc')
+                ->take($limit)
+                ->get(
+                    array(
+                        'post_id as id',
+                        'post_attachment as attachment',
+                        'post_text as text',
+                        'post_subject as subject',
+                        'post_checksum as checksum',
+                        'topic_id as topic_id',
+                        'post_time as time'
+                    )
+                );
+        }
+        else
+        {
+            return Post::query()
+                ->where('post_approved','=','1')
+                ->order_by('post_time', 'desc')
+                ->take($limit)
+                ->get(
+                    array(
+                        'post_id as id',
+                        'post_attachment as attachment',
+                        'post_text as text',
+                        'post_subject as subject',
+                        'post_checksum as checksum',
+                        'topic_id as topic_id',
+                        'post_time as time'
+                    )
+                );
+        }
     }
 
 }
