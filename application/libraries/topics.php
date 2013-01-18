9def2c7467d6e8a35f68e8c3ea003ca596064227
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-01-18 02:26:09 +0100

    Enchant the library topic possibilities

--- application/libraries/topics.php
+++ application/libraries/topics.php
@@ -1,21 +1,44 @@
 <?php
 class Topics {
-    public static function get($topicId)
+    public static function get($topicId, $limit = NULL)
     {
-        return Post::query()
-            ->where('topic_id','=', $topicId)
-            ->where('post_approved','=','1')
-            ->order_by('post_time')
-            ->get(
-                array(
-                    'post_id as id',
-                    'post_attachment as attachment',
-                    'post_text as text',
-                    'post_checksum as checksum',
-                    'poster_id as poster_id',
-                    'post_time as post_time'
-                )
-            );
+        if( $limit )
+        {
+            return Post::query()
+                ->where('topic_id','=', $topicId)
+                ->where('post_approved','=','1')
+                ->order_by('post_time')
+                ->get(
+                    array(
+                        'post_id as id',
+                        'post_attachment as attachment',
+                        'post_subject as subject',
+                        'post_text as text',
+                        'post_checksum as checksum',
+                        'poster_id as poster_id',
+                        'post_time as time'
+                    )
+                );
+        }
+        else
+        {
+            return Post::query()
+                ->where('topic_id','=', $topicId)
+                ->where('post_approved','=','1')
+                ->order_by('post_time', 'desc')
+                ->take($limit)
+                ->get(
+                    array(
+                        'post_id as id',
+                        'post_attachment as attachment',
+                        'post_subject as subject',
+                        'post_text as text',
+                        'post_checksum as checksum',
+                        'poster_id as poster_id',
+                        'post_time as time'
+                    )
+                );
+        }
     }
 }
 ?>
