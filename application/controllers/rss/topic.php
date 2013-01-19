eccc936df50de187f6076664c012d8b474db9781
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-01-19 22:54:06 +0100

    Use the common rss library

--- application/controllers/rss/topic.php
+++ application/controllers/rss/topic.php
@@ -10,27 +10,14 @@ class RSS_Topic_Controller extends Base_Controller
         foreach( $datas as $data)
         {
             $posts[$a] = $data->to_array();
+            $posts[$a]['topic_id'] = $topicId;
             $a = $a + 1;
         }
-        $xml = new SimpleXMLElement('<rss version="2.0" encoding="utf-8"></rss>');
-        $xml->addChild('channel'); 
-        $xml->channel->addChild('title', 'ViGLug Forum Posts'); 
-        $xml->channel->addChild('link', 'http://rss.forum.viglug.org/'); 
-        $xml->channel->addChild('description', 'Latest posts in ViGLug Forum');
-        $xml->channel->addChild('language', 'it-it'); 
-        $xml->channel->addChild('webMaster', 'fabiolocati@gmail.com');
-        $xml->channel->addChild('generator', 'PHPbbAPI');
-        $xml->channel->addChild('docs', 'http://www.viglug.org');
-        $xml->channel->addChild('pubDate', date(DATE_RSS)); 
-
-        foreach( $posts as $post) {
-            $item = $xml->channel->addChild('item'); 
-            $item->addChild('title', htmlentities($post['subject'])); 
-            $item->addChild('link', 'http://forum.viglug.org/viewtopic.php?t=' . $topicId . '#p' . $post['id']);
-            $item->addChild('description', htmlentities($post['text'])); 
-            $item->addChild('pubDate', date(DATE_RSS, $post['time'])); 
+        $xml = new Rsss('<rss version="2.0" encoding="utf-8"></rss>');
+        $xml->head();
+        foreach($posts as $val) {
+            $xml->entry($val);
         }
-//        array_walk_recursive($test_array, array ($xml, 'addChild'));
         return $xml->asXML();
     }
 }
