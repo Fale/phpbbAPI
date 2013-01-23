13dcc5d52a66b7bbad5c31812c950d01ed9419d9
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-01-23 11:43:54 +0100

    Move the RSSS::generateText function to the Texts class and abstract it more

--- application/libraries/rsss.php
+++ application/libraries/rsss.php
@@ -19,37 +19,8 @@ class Rsss extends SimpleXMLElement{
         $item = $this->channel->addChild('item'); 
         $item->addChild('title', htmlentities($val['subject'])); 
         $item->addChild('link', $this->generateUrl($val));
-        $item->addChild('description', $this->generateText($val)); 
+        $item->addChild('description', Texts::toUTF8($val['text'])); 
         $item->addChild('pubDate', date(DATE_RSS, $val['time'])); 
-    }
-
-    private function generateText($val)
-    {
-        $text = htmlentities($val['text']);
-        // Lists
-        $text = str_replace("[list=1:259zw1au]", "&lt;ul&gt;", $text);
-        $text = str_replace("[*:259zw1au]", "&lt;li&gt;", $text);
-        $text = str_replace("[/*:m:259zw1au]", "&lt;/li&gt;", $text);
-        $text = str_replace("[/list:o:259zw1au]", "&lt;/ul&gt;", $text);
-        // Lists ordered
-        $text = str_replace("[list=1:31vsco5w]", "&lt;ol&gt;", $text);
-        $text = str_replace("[*:31vsco5w]", "&lt;li&gt;", $text);
-        $text = str_replace("[/*:m:31vsco5w]", "&lt;/li&gt;", $text);
-        $text = str_replace("[/list:o:31vsco5w]", "&lt;/ol&gt;", $text);
-        // Italic
-        $text = str_replace("[i:259zw1au]", "&lt;i&gt;", $text);
-        $text = str_replace("[/i:259zw1au]", "&lt;/i&gt;", $text);
-        // emoticons
-        $text = str_replace("{SMILIES_PATH}", "http://forum.viglug.org/images/smilies", $text);
-        // Accented letters
-        $text = str_replace("&Atilde;&nbsp;", "&agrave;", $text); // à
-        $text = str_replace("&Atilde;&uml;", "&egrave;", $text); // è
-        $text = str_replace("&Atilde;&copy;", "&eacute;", $text); // é
-        $text = str_replace("&Atilde;&not;", "&igrave;", $text); // ì
-        $text = str_replace("&Atilde;&sup2;", "&ograve;", $text); // ò
-        $text = str_replace("&Atilde;&sup1;", "&ugrave;", $text); // ù
-        $text = utf8_encode($text);
-        return $text;
     }
 
     private function generateUrl($val)
---
+++ application/libraries/texts.php
@@ -0,0 +1,32 @@
+<?php
+class Texts {
+    public static function toUTF8($text)
+    {
+        $text = htmlentities($text);
+        // Lists
+        $text = str_replace("[list=1:259zw1au]", "&lt;ul&gt;", $text);
+        $text = str_replace("[*:259zw1au]", "&lt;li&gt;", $text);
+        $text = str_replace("[/*:m:259zw1au]", "&lt;/li&gt;", $text);
+        $text = str_replace("[/list:o:259zw1au]", "&lt;/ul&gt;", $text);
+        // Lists ordered
+        $text = str_replace("[list=1:31vsco5w]", "&lt;ol&gt;", $text);
+        $text = str_replace("[*:31vsco5w]", "&lt;li&gt;", $text);
+        $text = str_replace("[/*:m:31vsco5w]", "&lt;/li&gt;", $text);
+        $text = str_replace("[/list:o:31vsco5w]", "&lt;/ol&gt;", $text);
+        // Italic
+        $text = str_replace("[i:259zw1au]", "&lt;i&gt;", $text);
+        $text = str_replace("[/i:259zw1au]", "&lt;/i&gt;", $text);
+        // emoticons
+        $text = str_replace("{SMILIES_PATH}", "http://forum.viglug.org/images/smilies", $text);
+        // Accented letters
+        $text = str_replace("&Atilde;&nbsp;", "&agrave;", $text); // à
+        $text = str_replace("&Atilde;&uml;", "&egrave;", $text); // è
+        $text = str_replace("&Atilde;&copy;", "&eacute;", $text); // é
+        $text = str_replace("&Atilde;&not;", "&igrave;", $text); // ì
+        $text = str_replace("&Atilde;&sup2;", "&ograve;", $text); // ò
+        $text = str_replace("&Atilde;&sup1;", "&ugrave;", $text); // ù
+        $text = utf8_encode($text);
+        return $text;
+    }
+}
+?>
