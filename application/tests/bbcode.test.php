794582e54887acf480cfd44c9f45aca8a46ed799
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-01-29 11:40:10 +0100

    Add second test

--- application/tests/bbcode.test.php
+++ application/tests/bbcode.test.php
@@ -12,4 +12,14 @@ class TestBBCode extends PHPUnit_Framework_TestCase {
 		$this->assertEquals("[quote]", $cleaned);	
 	}
 
+    /**
+	 * Test that a simple PHPbb quote works properly
+	 * @return void
+	 */
+	public function testSimpleQuote()
+	{
+        $cleaned = BBCode::toStdBBCode("[quote:35s4h6he]");
+		$this->assertEquals("[quote]", $cleaned);	
+	}
+
 }
