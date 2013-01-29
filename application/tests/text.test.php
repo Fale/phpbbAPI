39a997d38551996cc5eeeeade82bc795d6859e6c
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-01-29 13:23:26 +0100

    Add simple test for Text

---
+++ application/tests/text.test.php
@@ -0,0 +1,15 @@
+<?php
+
+class TestText extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Test that a string to be excaped works properly
+	 * @return void
+	 */
+	public function testString()
+	{
+        $cleaned = Texts::toUTF8("è");
+		$this->assertEquals("&egrave;", $cleaned);	
+	}
+
+}
