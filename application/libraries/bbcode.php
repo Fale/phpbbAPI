e719547474d7664422e1cf46ef09ae370d9a9510
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-01-30 23:10:01 +0100

    Fix quote quotes

--- application/libraries/bbcode.php
+++ application/libraries/bbcode.php
@@ -6,12 +6,17 @@ class BBCode {
         return preg_replace('/\[(\/?)([^:]*):([^\]]*)\]/i','[$1$2]', $text);
     }
 
+    public static function toStdQuote($text)
+    {
+        return preg_replace('/\[quote(=?)(&amp;quot;?)([^\]]*)(&amp;quot;?)\]/i','[quote=$3]', $text);
+    }
+
     public static function toHTML($text, $clean = TRUE)
     {
         if ($clean)
             $text = Text::toUTF8($text);
         $text = BBCode::toStdBBCode($text);
-
+        $text = BBCode::toStdQuote($text);
         $parser = new JBBCode\Parser();
         $parser->loadDefaultCodes();
         $parser->addBBCode('quote','<blockquote><p>{param}</p></blockquote>');
