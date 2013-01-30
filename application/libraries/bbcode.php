f4cd3dbdd98562c70e60712a601b83c406d8a6ef
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-01-30 23:17:27 +0100

    Add BBCode::fixEmoticonsURL function

--- application/libraries/bbcode.php
+++ application/libraries/bbcode.php
@@ -11,12 +11,18 @@ class BBCode {
         return preg_replace('/\[quote(=?)(&amp;quot;?)([^\]]*)(&amp;quot;?)\]/i','[quote=$3]', $text);
     }
 
+    public static function fixEmoticonsURL($text)
+    {
+        return str_replace("{SMILIES_PATH}", "http://forum.viglug.org/images/smilies", $text);
+    }
+
     public static function toHTML($text, $clean = TRUE)
     {
         if ($clean)
             $text = Text::toUTF8($text);
         $text = BBCode::toStdBBCode($text);
         $text = BBCode::toStdQuote($text);
+        $text = BBCode::fixEmoticonsURL($text);
         $parser = new JBBCode\Parser();
         $parser->loadDefaultCodes();
         $parser->addBBCode('quote','<blockquote><p>{param}</p></blockquote>');
