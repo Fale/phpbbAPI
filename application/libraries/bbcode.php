0a2d48ed7ceeaa696499088f838a509a9c75b52b
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-01-30 23:27:00 +0100

    Manage properly the lists

--- application/libraries/bbcode.php
+++ application/libraries/bbcode.php
@@ -27,6 +27,9 @@ class BBCode {
         $parser->loadDefaultCodes();
         $parser->addBBCode('quote','<blockquote><p>{param}</p></blockquote>');
         $parser->addBBCode('quote','<b>{option} wrote:</b><blockquote><p>{param}</p></blockquote>', true);
+        $parser->addBBCode('list','<ul>{param}</ul>');
+        $parser->addBBCode('list','<ol type="{option}">{param}</ol>', true);
+        $parser->addBBCode('*','<li>{param}</li>');
         $parser->addBBCode('code', '<code>{param}</code>', false, false, 1);
         $parser->parse( $text );
         return $parser->getAsHtml();
