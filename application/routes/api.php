5c58a5e1140c6fcade745c4ee1bc11c3d9c7a7ba
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-01-31 01:36:57 +0100

    Initiate to work on the login

--- application/routes/api.php
+++ application/routes/api.php
@@ -13,6 +13,7 @@ Route::get('v0', function()
 
 Route::any('v0/forum/(:num?)', array('uses' => 'v0.forum@index'));
 Route::any('v0/user/(:num?)', array('uses' => 'v0.user@index'));
+Route::any('v0/login/(:any?)/(:any?)', array('uses' => 'v0.login@index'));
 Route::any('v0/topic/(:num)', array('uses' => 'v0.topic@index'));
 Route::any('v0/stats/(:any?)', array('uses' => 'v0.stat@index'));
 ?>
