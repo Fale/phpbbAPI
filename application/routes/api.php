30da089f53052e83b9bee43399484a2ce6a2f395
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-02-01 19:49:47 +0100

    Add specific errors for API

--- application/routes/api.php
+++ application/routes/api.php
@@ -15,4 +15,14 @@ Route::any('v0/forum/(:num?)', array('uses' => 'v0.forum@index'));
 Route::any('v0/user/(:num?)', array('uses' => 'v0.user@index'));
 Route::any('v0/topic/(:num)', array('uses' => 'v0.topic@index'));
 Route::any('v0/stats/(:any?)', array('uses' => 'v0.stat@index'));
+
+Event::listen('404', function()
+{
+    return Response::json( array( 'errors' => array( 'message' => 'The requested resource could not be found', 'code' => '404')), '404');
+});
+
+Event::listen('500', function()
+{
+    return Response::json( array( 'errors' => array( 'message' => 'Internal Server Error', 'code' => '500')), '500');
+});
 ?>
