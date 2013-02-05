0627a27abfdc55e0221f17bfb87d3e8381b8a169
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-02-05 20:14:07 +0100

    Huge changes in the dev.DOMAIN subdomain

--- application/routes/dev.php
+++ application/routes/dev.php
@@ -1,11 +1,16 @@
 <?php
 // HOME
 Route::get('/', function() {
-    return View::make('api.index');
+    return View::make('dev.index');
+});
+
+// API
+Route::get('api', function() {
+    return View::make('dev.api');
 });
 
 // API V0
-Route::get('v0', function() {
-    return View::make('v0.index');
+Route::get('api/v0', function() {
+    return View::make('api-v0.index');
 });
 ?>
---
+++ application/views/api-v0/index.blade.php
@@ -0,0 +1,21 @@
+@include('dev.header')
+@include('dev.api-v0-nav')
+        <div class="span9">
+          <div class="hero-unit">
+            <h1>Hello, developer!</h1>
+            <p>Here you'll find all the information you'll need about the PHPbb developement.</p>
+          </div>
+          <div class="row-fluid">
+            <div class="span4">
+              <h2>API</h2>
+              <p>Here you'll find all the information you need about the API system. </p>
+              <p><a class="btn" href="/api">View details &raquo;</a></p>
+            </div><!--/span-->
+            <div class="span4">
+              <h2>RSS</h2>
+              <p>Here you'll find all the information you need to get the RSS up. </p>
+              <p><a class="btn" href="/rss">View details &raquo;</a></p>
+            </div><!--/span-->
+          </div><!--/row-->
+        </div><!--/span-->
+@include('dev.footer')
