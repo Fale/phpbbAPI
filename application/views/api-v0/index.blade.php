f1f49803be7a179539e8742c227519f13ece8227
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-02-05 20:29:36 +0100

    Improve dev.forum.viglug.org/api/v0

--- application/views/api-v0/index.blade.php
+++ application/views/api-v0/index.blade.php
@@ -2,20 +2,8 @@
 @include('api-v0.navigation')
         <div class="span9">
           <div class="hero-unit">
-            <h1>Hello, developer!</h1>
-            <p>Here you'll find all the information you'll need about the PHPbb developement.</p>
+            <h1>API v0</h1>
+            <p>This is the documentation of the first version of the PHPbb API. As the name (v0) suggest, they are definitely not ready for any kind of complete task. This because they are <strong>read-only API</strong>.</p>
           </div>
-          <div class="row-fluid">
-            <div class="span4">
-              <h2>API</h2>
-              <p>Here you'll find all the information you need about the API system. </p>
-              <p><a class="btn" href="/api">View details &raquo;</a></p>
-            </div><!--/span-->
-            <div class="span4">
-              <h2>RSS</h2>
-              <p>Here you'll find all the information you need to get the RSS up. </p>
-              <p><a class="btn" href="/rss">View details &raquo;</a></p>
-            </div><!--/span-->
-          </div><!--/row-->
         </div><!--/span-->
 @include('dev.footer')
