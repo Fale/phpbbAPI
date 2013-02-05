00024ef3f9d4ff56648391133929415907927cae
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-02-05 21:56:04 +0100

    Improve navigation

--- application/views/api-v0/navigation.blade.php
+++ application/views/api-v0/navigation.blade.php
@@ -1,11 +1,8 @@
         <div class="span3">
           <div class="well sidebar-nav">
             <ul class="nav nav-list">
-              <li class="nav-header">Forum</li>
-              <li class="active"><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
+              <li class="nav-header"><a href="/api/v0/forum">Forum</a></li>
+              <li><a href="/api/v0/forum#get">GET</a></li>
               <li class="nav-header">User</li>
               <li><a href="#">Link</a></li>
               <li><a href="#">Link</a></li>
