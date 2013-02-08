5e712fe52db74d012eee5a09aa24aa61664679ad
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-02-08 17:33:30 +0100

    Superseed libraries/forums.php and remove it

--- application/controllers/v0/forum.php
+++ application/controllers/v0/forum.php
@@ -5,6 +5,19 @@ class V0_Forum_Controller extends Base_Controller
 
     public function get_index($forumId = NULL)
     {
-        return Response::eloquent(Forums::get($forumId));
+        if($forumId)
+            return Response::eloquent(
+                Topic::query()
+                    ->where('forum_id','=', $forumId)
+                    ->where('topic_approved','=','1')
+                    ->order_by('topic_last_post_time', 'desc')
+                    ->get(Topic::mask())
+            );
+        else
+            return Response::eloquent(
+                Forum::query()
+                    ->order_by('left_id')
+                    ->get(Forum::mask())
+            );
     }
 }
--- application/models/topic.php
+++ application/models/topic.php
@@ -1,5 +1,26 @@
 <?php
 class Topic extends Eloquent {
     public static $key = 'topic_id';
+
+    private static $public = Array(
+        'topic_id as id',
+        'topic_attachment as attachment',
+        'topic_title as title',
+        'topic_views as views',
+        'topic_replies as topic_replies',
+        'topic_first_poster_name as first_poster',
+        'topic_poster as first_poster_id',
+        'topic_time as first_post_time',
+        'topic_last_poster_name as last_poster',
+        'topic_last_poster_id as last_poster_id',
+        'topic_last_post_time as last_post_time'
+    );
+
+    private static $private = Array();
+
+    public static function mask()
+    {
+        return Topic::$public;
+    }
 }
 ?>
