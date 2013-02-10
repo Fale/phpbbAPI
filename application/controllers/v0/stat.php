d1a9f2f0a6d19f3d610307b666b2288d8b8a829e
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-02-10 10:31:53 +0100

    Move control code to the controller instead of a lonely library

--- application/controllers/v0/stat.php
+++ application/controllers/v0/stat.php
@@ -5,6 +5,21 @@ class V0_Stat_Controller extends Base_Controller
 
     public function get_index($object = NULL)
     {
-        return "[" . json_encode(Stats::get($object)) . "]";
+        $output = Array();
+        if( !$object || $object == "posts")
+            $output = array_merge( $output, Array("posts" => Post::query()->count()));
+        if( !$object || $object == "users")
+            $output = array_merge( $output, Array("users" => User::query()->count()));
+        if( !$object || $object == "topics")
+            $output = array_merge( $output, Array("topics" => Topic::query()->count()));
+        if( !$object || $object == "most-replied-topic")
+        {
+            $t = Topic::query()->order_by('topic_replies','desc')->take(1)->get('topic_id');
+            $temp = Array("most-replied-topic" => $t['0']->to_array()['topic_id']);
+            $output = array_merge( $output, $temp);
+        }
+        if( !$object || $object == "forums")
+            $output = array_merge( $output, Array("forums" => Forum::query()->count()));
+        return "[" . json_encode($output) . "]";
     }
 }
