562f25f8cdf3ba171247ad6b0ba32300c1021e66
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-02-13 15:22:03 +0100

    Improve the quality of the navigation system in dev.@/api/v0/* making the links active only when they really are

--- application/views/api-v0/navigation.blade.php
+++ application/views/api-v0/navigation.blade.php
@@ -1,21 +1,21 @@
         <div class="span3">
           <div class="well sidebar-nav">
             <ul class="nav nav-list">
-              <li><a href="/api/v0/forum">Forum</a></li>
+              <li class="{{ URI::is('/api/v0/forum') ? 'active' : '' }}"><a href="/api/v0/forum">Forum</a></li>
               <ul>
-                <li><a href="/api/v0/forum#get">GET</a></li>
+                <li class="{{ URI::is('/api/v0/forum#get') ? 'active' : '' }}"><a href="/api/v0/forum#get">GET</a></li>
               </ul>
-              <li><a href="/api/v0/stat">Stat</a></li>
+              <li class="{{ URI::is('/api/v0/stat') ? 'active' : '' }}"><a href="/api/v0/stat">Stat</a></li>
               <ul>
-                <li><a href="/api/v0/stat#get">GET</a></li>
+                <li class="{{ URI::is('/api/v0/stat#get') ? 'active' : '' }}"><a href="/api/v0/stat#get">GET</a></li>
               </ul>
-              <li><a href="/api/v0/topic">Topic</a></li>
+              <li class="{{ URI::is('/api/v0/topic') ? 'active' : '' }}"><a href="/api/v0/topic">Topic</a></li>
               <ul>
-                <li><a href="/api/v0/topic#get">GET</a></li>
+                <li class="{{ URI::is('/api/v0/topic#get') ? 'active' : '' }}"><a href="/api/v0/topic#get">GET</a></li>
               </ul>
-              <li><a href="/api/v0/user">User</a></li>
+              <li class="{{ URI::is('/api/v0/user') ? 'active' : '' }}"><a href="/api/v0/user">User</a></li>
               <ul>
-                <li><a href="/api/v0/user#get">GET</a></li>
+                <li class="{{ URI::is('/api/v0/user#get') ? 'active' : '' }}"><a href="/api/v0/user#get">GET</a></li>
               </ul>
             </ul>
           </div><!--/.well -->
