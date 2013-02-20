a3aa4924a759bb2cd1b7ab9fbb7d52f3d63fbcfb
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-02-20 01:40:34 +0100

    Add more contact ways

--- application/views/dev/contact.blade.php
+++ application/views/dev/contact.blade.php
@@ -6,6 +6,8 @@
             <ul>
               <li><a href="mailto:fabiolocati@gmail.com">Email</a></li>
               <li><a href="https://plus.google.com/100271380639248552621">Google+</a></li>
+              <li><a href="http://www.linkedin.com/in/fabiolocati">LinkedIn</a></li>
+              <li><a href="https://twitter.com/f4l3">Twitter</a></li>
               <li><a href="http://fabiolocati.com">Website</a></li>
             </ul>
           </div>
