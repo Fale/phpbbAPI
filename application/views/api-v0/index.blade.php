1206cef06bdb75b80a1b2cbdb97db3dcfcfc8928
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-03-11 21:32:43 +0100

    Let's talk about APIv1

--- application/views/api-v0/index.blade.php
+++ application/views/api-v0/index.blade.php
@@ -5,5 +5,7 @@
             <h1>API v0</h1>
             <p>This is the documentation of the first version of the PHPbb API. As the name (v0) suggest, they are definitely not ready for any kind of complete task. This because they are <strong>read-only API</strong>.</p>
           </div>
+            <h1>API v1</h1>
+            <p>These will be full-features API and will be available soon.</p>
         </div><!--/span-->
 @include('dev.footer')
